6ee0e086f6fa881f91de6f8e021aab6b7d841650
kerem <kerembuyukhamurkar@gmail.com git config --global user.email kerembuyukhamurkar@g>
2023-04-09 16:40:00 +0300

    Add project files.

---
+++ MyFirstGitProject/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MyFirstGitProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
